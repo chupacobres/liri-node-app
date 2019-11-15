fe9bdf36adaa31b0130f0086872c5d60700927ec
juan uribe <juanpablouribe@mac.com>
2019-11-14 20:47:22 -0500

    added first function for concerts. returns all data, need to loop through objects

--- liri.js
+++ liri.js
@@ -1,3 +1,53 @@
 require("dotenv").config();
 
-var keys = require("./keys.js");
+var fs = require("fs");
+
+var axios = require("axios");
+
+var keys = require("./key.js");
+
+var moment = require("moment");
+
+var Spotify = require("node-spotify-api");
+
+var action = process.argv[2];
+var value = process.argv[3];
+
+switch (action) {
+    case "concert-this":
+        concertThis();
+        break;
+
+    case "spotify-this-song":
+        spotifyThisSong();
+        break;
+
+    case "movie-this":
+        movieThis();
+        break;
+
+    case "do-what-it-says":
+        doWhatItSays();
+        break;
+}
+
+function concertThis() {
+    axios.get("https://rest.bandsintown.com/artists/" + value + "/events?app_id=codingbootcamp").then(
+        function (response) {
+            console.log(response.data);
+        },
+
+        function (error) {
+            if (error.response) {
+                console.log(error.response.data);
+                console.log(error.response.status);
+                console.log(error.response.headers);
+            } else if (error.request) {
+                console.log(error.request);
+            } else {
+                console.log("Error", error.message);
+            }
+            console.log(error.config);
+        }
+    );
+}

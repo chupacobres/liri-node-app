3b51d3232c78664dd80f673f4953ea83f4f7a118
juan uribe <juanpablouribe@mac.com>
2019-11-16 23:24:54 -0500

    app is fully functional, working on readme.md

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # liri-node-app
-A command line app using node.js that gives you back data from 3 API's
+A command line app using node.js that pulls data from 3 API's
+
+### How it works
+LIRI (Inspired from Apple's Siri), pulls data from Bands in Town API, Node Spotify and OMDB using npm packages and axios. You can get information on touring bands, movies and songs using this app.
+
+Once the App runs (by navigating to the file's root and runnin node liri.js), you can choose among these 3 actions:
+* concert-this <artist> (This will pull show dates of the artist if on tour)
+* spotify-this-song <song> (This will give you info on the song provided)
+* movie-this <movie> (This will give you info on the movie provided)
+* do-what-it-says (This will run any of the previous commands from a random.txt file found in the same directory)
+<h3>example: spotify-this-song thriller<h3>
+
+Keep in mind that cloning this app would only let you pull data using concert-this and movie-this. You would need to create a .env file with your own Spotify Developer credentials. Also, mode nodules are not tracked on github, so make sure you install axios, moment, dotenv and spotify's npm.
+
+
+
+
--- liri.js
+++ liri.js
@@ -20,8 +20,8 @@ for (var i = 3; i < nodeArgs.length; i++) {
 }
 value = valueArr.join(" ");
 handleUser(action, value);
-function handleUser(action, value){
-    
+function handleUser(action, value) {
+
 
     switch (action) {
         case "concert-this":
@@ -99,6 +99,7 @@ function movieThis() {
             console.log("Language(s): " + response.data.Language);
             console.log("Plot: " + response.data.Plot)
             console.log("Actors: " + response.data.Actors)
+
         },
         function (error) {
             if (error.response) {
@@ -121,14 +122,74 @@ function doWhatItSays() {
         if (error) {
             return console.log(error);
         }
-
-        console.log(data);
-
         var dataArr = data.split(",");
+        if (dataArr[0] == "movie-this") {
+            axios.get("http://www.omdbapi.com/?apikey=trilogy&t=" + dataArr[1]).then(
+                function (response) {
+                    console.log("Title: " + response.data.Title);
+                    console.log("Year: " + response.data.Year);
+                    console.log("IMDB Rating: " + response.data.imdbRating);
+                    console.log(response.data.Ratings[1].Source, response.data.Ratings[1].Value);
+                    console.log("Production Country(ies): " + response.data.Country);
+                    console.log("Language(s): " + response.data.Language);
+                    console.log("Plot: " + response.data.Plot)
+                    console.log("Actors: " + response.data.Actors)
 
-        console.log(dataArr);
+                },
+                function (error) {
+                    if (error.response) {
+                        console.log(error.response.data);
+                        console.log(error.response.status);
+                        console.log(error.response.headers);
+                    } else if (error.request) {
+                        console.log(error.request);
+                    } else {
+                        console.log("Error", error.message);
+                    }
+                    console.log(error.config);
+                }
+            );
+        }
+        else if (dataArr[0] == "concert-this") {
+            axios.get("https://rest.bandsintown.com/artists/" + dataArr[1] + "/events?app_id=codingbootcamp").then(
+                function (response) {
+                    for (i = 0; i < response.data.length; i++) {
 
-        handleUser(...dataArr)
+                        console.log(response.data[i].venue.name + " in " + response.data[i].venue.city + ", " + response.data[i].venue.country + " on " + moment(response.data[i].datetime).format('MM/DD/YYYY'));
+                    }
+                },
+                function (error) {
+                    if (error.response) {
+                        console.log(error.response.data);
+                        console.log(error.response.status);
+                        console.log(error.response.headers);
+                    } else if (error.request) {
+                        console.log(error.request);
+                    } else {
+                        console.log("Error", error.message);
+                    }
+                    console.log(error.config);
+                }
+            );
+        }
+        else if (dataArr[0] == "spotify-this-song") {
+            var spotify = new Spotify({
+                id: process.env.SPOTIFY_ID,
+                secret: process.env.SPOTIFY_SECRET
+            });
+
+            spotify
+                .search({ type: 'track', query: dataArr[1] })
+                .then(function (response) {
+                    console.log("Artist: " + response.tracks.items[0].album.artists[0].name);
+                    console.log("Album: " + response.tracks.items[0].album.name);
+                    console.log("Release date: " + response.tracks.items[0].album.release_date);
+                    console.log("Preview URL: " + response.tracks.items[0].preview_url)
+                })
+                .catch(function (err) {
+                    console.log(err);
+                });
+        }
     });
 
 }
